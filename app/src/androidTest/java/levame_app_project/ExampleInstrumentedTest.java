c9118d58d80fad0c5a2df2933df078763f5569f8
MauroFab <maurotoscano2@gmail.com>
2017-09-03 20:08:22 -0300

    Agregado el facebook SDK. Puesto para que se busque en el repositorio de mavenCentral

--- app/src/androidTest/java/levame_app_project/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/levame_app_project/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.mauro.llevame_app_project", appContext.getPackageName());
+        assertEquals("llevame_app_project", appContext.getPackageName());
     }
 }
