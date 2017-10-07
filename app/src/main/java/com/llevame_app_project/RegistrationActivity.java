bfd0f928210c695b2d09b1cf3f3f6fb9c5264a4c
MauroFab <maurotoscano2@gmail.com>
2017-10-07 14:25:54 -0300

    Finished the card registtration form

--- app/src/main/java/com/llevame_app_project/RegistrationActivity.java
+++ app/src/main/java/com/llevame_app_project/RegistrationActivity.java
@@ -1,12 +1,15 @@
 package com.llevame_app_project;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.view.View.OnClickListener;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -17,6 +20,14 @@ public class RegistrationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Llevame - Registration");
+
+        Button mNextStepButton = (Button) findViewById(R.id.next_step_button);
+        mNextStepButton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RegistrationActivity.this, CardRegistrationActivity.class));
+            }
+        });
 
 
     }
