d1a504c39b73b8ce22aefd04ad33a39092ed2c87
MauroFab <maurotoscano2@gmail.com>
2017-10-07 01:12:30 -0300

    Refactor al validator

--- app/src/main/java/com/llevame_app_project/LoginValidator.java
+++ app/src/main/java/com/llevame_app_project/LoginValidator.java
@@ -7,12 +7,10 @@ class LoginValidator {
     }
 
     public boolean isEmailValid(String email) {
-        //TODO: Replace this with your own logic
         return email.contains("@");
     }
 
     public boolean isPasswordValid(String password) {
-        //TODO: Replace this with your own logic
         return password.length() > 4;
     }
 }
