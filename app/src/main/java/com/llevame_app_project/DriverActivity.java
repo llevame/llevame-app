ad2a39317a9dc912d2da9886fcc0b1657606366a
MauroFab <maurotoscano2@gmail.com>
2017-10-07 16:12:53 -0300

    Added an empty DriverActivity

---
+++ app/src/main/java/com/llevame_app_project/DriverActivity.java
@@ -0,0 +1,15 @@
+package com.llevame_app_project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mauro.com.com.com.com.llevame_app_project.R;
+
+public class DriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver);
+    }
+}
