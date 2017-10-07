99518a049c7ed14093aaee57a955e77b4c0c8253
MauroFab <maurotoscano2@gmail.com>
2017-10-07 17:00:02 -0300

    Added a toolbar to the DriverActivity, with the option to the profile settings

--- app/src/main/java/com/llevame_app_project/CarRegistrationActivity.java
+++ app/src/main/java/com/llevame_app_project/CarRegistrationActivity.java
@@ -1,7 +1,10 @@
 package com.llevame_app_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CarRegistrationActivity extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class CarRegistrationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_registration);
+
+        Button mFinishButton = (Button) findViewById(R.id.finish_button);
+        mFinishButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CarRegistrationActivity.this,
+                        DriverActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/llevame_app_project/DriverActivity.java
+++ app/src/main/java/com/llevame_app_project/DriverActivity.java
@@ -1,7 +1,11 @@
 package com.llevame_app_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class DriverActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class DriverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
+        getSupportActionBar().setTitle("Llevame - Driver");
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_driver, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.profile_settings) {
+            startActivity(new Intent(DriverActivity.this, ProfileActivity.class));
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
