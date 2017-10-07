42e653dcd6d4f4725510269c365ce3b96992449b
MauroFab <maurotoscano2@gmail.com>
2017-10-07 03:13:10 -0300

    Armada la primer pantalla del formulario para registrarse

--- app/src/main/java/com/llevame_app_project/RegistrationActivity.java
+++ app/src/main/java/com/llevame_app_project/RegistrationActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RadioButton;
 
 public class RegistrationActivity extends AppCompatActivity {
 
@@ -16,6 +17,22 @@ public class RegistrationActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setTitle("Llevame - Registration");
+
+
     }
 
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.passengerRadioButton:
+                if (checked)
+                    break;
+            case R.id.driverRadioButton:
+                if (checked)
+                    break;
+        }
+    }
 }
