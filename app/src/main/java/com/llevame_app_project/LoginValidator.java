6d74835484c8a436d9dfb262cd4455428a651ea1
MauroFab <maurotoscano2@gmail.com>
2017-10-09 19:19:21 -0300

    Added documentation

--- app/src/main/java/com/llevame_app_project/LoginValidator.java
+++ app/src/main/java/com/llevame_app_project/LoginValidator.java
@@ -5,11 +5,11 @@ class LoginValidator {
     LoginValidator(){
 
     }
-
+    /**Checks if the email contains an @ symbol*/
     public boolean isEmailValid(String email) {
         return email.contains("@");
     }
-
+    /**Checks if the password contains more than 4 characters*/
     public boolean isPasswordValid(String password) {
         return password.length() > 4;
     }
