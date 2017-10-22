28d9ac18664d8e31c626810d6da20438dfdf6cdc
MauroFab <maurotoscano2@gmail.com>
2017-10-22 20:56:42 -0300

    Added a connection between the LoginAsyncTask and the activity, setted it so the spinners appears while waiting for the server response

--- app/src/main/java/com/llevame_app_project/AsyncLoginTask.java
+++ app/src/main/java/com/llevame_app_project/AsyncLoginTask.java
@@ -1,24 +1,31 @@
 package com.llevame_app_project;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.os.AsyncTask;
-import retrofit2.http.GET;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+import android.widget.Toast;
+import com.llevame_app_project.LoginActivity;
+class AsyncLoginTask extends AsyncTask<String, String, Boolean>{
 
+    private LoginActivity loginActivity;
 
-class AsyncLoginTask extends AsyncTask<String, String, String>{
+    public AsyncLoginTask(LoginActivity aLoginActivity){
+        this.loginActivity = aLoginActivity;
+    }
+
     @Override
     protected void onPreExecute() {
+        super.onPreExecute();
     }
 
     @Override
-    protected String doInBackground(String... params) {
-        //@GET("https://safe-savannah-49286.herokuapp.com/api/v1/users")
-
-        return "";
+    protected Boolean doInBackground(String... params) {
+        return false;
     }
 
     @Override
-    protected void onPostExecute(String s) {
-        //Show the result obtained from doInBackground
+    protected void onPostExecute(Boolean succesfullyConnection) {
+        if(!succesfullyConnection)
+            loginActivity.loginFinishedWithWrongCredentials();
+        loginActivity.loginFinishedSuccessfully("aToken");
     }
 }
--- app/src/main/java/com/llevame_app_project/LoginActivity.java
+++ app/src/main/java/com/llevame_app_project/LoginActivity.java
@@ -49,6 +49,7 @@ import com.llevame_app_project.MockupServer;
 import android.util.Log;
 import android.widget.Toast;
 import static android.Manifest.permission.READ_CONTACTS;
+import static java.lang.Thread.sleep;
 
 /**
  * A login screen that offers login via email/password.
@@ -59,17 +60,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * Id to identity READ_CONTACTS permission request.
      */
     private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * A dummy authentication store containing known user names and passwords.
-     * TODO: remove after connecting to a real authentication system.
-     */
-    private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
-    };
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
 
     LoginValidator loginValidator;
 
@@ -248,10 +238,12 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
-            showProgress(true);
-            MockupServer server = new MockupServer();
+            AsyncLoginTask loginTask = new AsyncLoginTask(this);
+            this.showProgress(true);
+            loginTask.execute();
+            /*MockupServer server = new MockupServer();
             if(server.attemptLogin(email,password)){
-                Log.i("FB_LOGIN", "Successfully logged in our servers");
+                Log.i("LOGIN", "Successfully logged in our servers");
                 startActivity(new Intent(LoginActivity.this, PassengerActivity.class));
             }else{
                 showProgress(false);
@@ -259,7 +251,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 focusView.requestFocus();
                 Toast.makeText(this.getBaseContext(),"Wrong password or email"
                                                 ,Toast.LENGTH_SHORT).show();
-            }
+            }*/
         }
     }
 
@@ -351,6 +343,15 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         int ADDRESS = 0;
         int IS_PRIMARY = 1;
+    }
+
+    public void loginFinishedWithWrongCredentials(){
+        this.showProgress(false);
+        Toast.makeText(this.getBaseContext(), "Wrong username or password", Toast.LENGTH_SHORT).show();
+    }
+
+    public void loginFinishedSuccessfully(String Token){
+        this.showProgress(false);
     }
 }
 
