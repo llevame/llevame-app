b509494c0c85176c5af1fab11b0e336f72f7168a
MauroFab <maurotoscano2@gmail.com>
2017-11-04 14:27:22 -0300

    Updated the login to work with the new changes of the API, GET ---> PATCH

--- app/src/main/java/com/llevame_app_project/Data/Remote/LoginServices.java
+++ app/src/main/java/com/llevame_app_project/Data/Remote/LoginServices.java
@@ -2,12 +2,17 @@ package com.llevame_app_project.Data.Remote;
 
 import com.llevame_app_project.Data.LoginResponseData;
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.PATCH;
 import retrofit2.http.Path;
 
 public interface LoginServices {
-    @GET("api/v1/account/{userName}")
+    @FormUrlEncoded
+    @PATCH("api/v1/account/{userName}")
     Call<LoginResponseData> loginUser(@Path("userName") String userName,
-                                      @Header("Password") String password);
+                                      @Field("password") String password);
 }
