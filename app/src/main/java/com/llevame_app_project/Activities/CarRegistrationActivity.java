a6ccac2a8d1e9ded91d219b1092753fd4f91b971
MauroFab <maurotoscano2@gmail.com>
2017-11-11 20:47:11 -0300

    Received the data to make a driver registration in the registration activity

--- app/src/main/java/com/llevame_app_project/Activities/CarRegistrationActivity.java
+++ app/src/main/java/com/llevame_app_project/Activities/CarRegistrationActivity.java
@@ -5,17 +5,39 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
+import com.llevame_app_project.Forms.FirstRegistrationForm;
+import com.llevame_app_project.Forms.SecondRegistrationForm;
 import com.llevame_app_project.R;
 
 public class CarRegistrationActivity extends AppCompatActivity {
+
+    private EditText mModel;
+    private EditText mPatent;
+    private EditText mYear;
+    private EditText mColor;
+    private CheckBox cAc;
+    private FirstRegistrationForm firstForm;
+    private SecondRegistrationForm secondForm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_car_registration);
 
-        Button mFinishButton = (Button) findViewById(R.id.finish_button);
+        mModel = findViewById(R.id.textModel);
+        mColor = findViewById(R.id.textColor);
+        mPatent = findViewById(R.id.textPatent);
+        mYear = findViewById(R.id.textYear);
+        cAc = findViewById(R.id.checkBoxAc);
+
+        firstForm = (FirstRegistrationForm) getIntent().getSerializableExtra("firstForm");
+        secondForm = (SecondRegistrationForm) getIntent().getSerializableExtra("secondForm");
+
+
+        Button mFinishButton = findViewById(R.id.finish_button);
         mFinishButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,5 +46,7 @@ public class CarRegistrationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
     }
 }
