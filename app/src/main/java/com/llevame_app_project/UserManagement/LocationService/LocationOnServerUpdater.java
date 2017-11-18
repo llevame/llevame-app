5fa2b0e2e5824fba96478009079101c0e495b155
MauroFab <maurotoscano2@gmail.com>
2017-11-18 20:23:02 -0300

    Added a service to listen to the GPS changes, added a thread inside it to report the position to the server (Currently just printing the location every X seconds)

---
+++ app/src/main/java/com/llevame_app_project/UserManagement/LocationService/LocationOnServerUpdater.java
@@ -0,0 +1,51 @@
+package com.llevame_app_project.UserManagement.LocationService;
+import android.annotation.SuppressLint;
+import android.location.Location;
+import android.util.Log;
+
+import com.google.android.gms.location.LocationServices;
+
+public class LocationOnServerUpdater extends Thread {
+
+
+    //If you use the app context nothing bad happens
+    @SuppressLint("StaticFieldLeak")
+    private static LocationOnServerUpdater daemon;
+
+    public static LocationOnServerUpdater getInstance(){
+        if(daemon == null) {
+            daemon = new LocationOnServerUpdater();
+        }
+        return daemon;
+    }
+
+    private LocationOnServerUpdater(){
+
+    }
+
+    Location lastKnownLocation;
+    private final Object lock = new Object();
+
+    @Override
+    public void run(){
+        //noinspection InfiniteLoopStatement
+        while(true){
+            if(lastKnownLocation != null){
+                synchronized (lock) {
+                    Log.i("LocationUpdater:", "Updating gps to:" + lastKnownLocation);
+                }
+            }
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void updateLocation(Location location){
+        synchronized (lock) {
+            lastKnownLocation = location;
+        }
+    }
+}
