e3dd158f6e705262072bab82ae28765efcadff88
MauroFab <maurotoscano2@gmail.com>
2017-11-19 18:17:32 -0300

    Fixed a bug in the service

--- app/src/main/java/com/llevame_app_project/Data/Remote/UserPatchServices.java
+++ app/src/main/java/com/llevame_app_project/Data/Remote/UserPatchServices.java
@@ -5,11 +5,12 @@ import android.location.Location;
 
 import com.llevame_app_project.Data.LoginResponseData;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.Header;
 import retrofit2.http.PATCH;
 
 public interface UserPatchServices {
     @PATCH("api/v1/account/me")
     Call<LoginResponseData> notifyLocation(@Header("Authorization") String BearerPlusToken,
-                                           Location location);
+                                           @Body Location location);
 }
