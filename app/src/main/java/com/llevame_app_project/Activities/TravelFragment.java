f4e614d56cb7d99e5aa8399edc5bbaffe45a3fa8
MauroFab <maurotoscano2@gmail.com>
2017-11-20 18:29:47 -0300

    Added the map

--- app/src/main/java/com/llevame_app_project/Activities/TravelFragment.java
+++ app/src/main/java/com/llevame_app_project/Activities/TravelFragment.java
@@ -6,6 +6,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
 import com.llevame_app_project.R;
 
 /**
@@ -15,6 +18,8 @@ public class TravelFragment extends Fragment {
 
     public TravelFragment() {
     }
+    private MapView mMapView;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +27,16 @@ public class TravelFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_travel, container, false);
         TextView textView = (TextView) rootView.findViewById(R.id.section_label);
         textView.setText("Soy la vista con el viaje a hacer");
+        mMapView = rootView.findViewById(R.id.mapViewTravel);
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return rootView;
     }
 }
